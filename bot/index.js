7b0574d71c1a5e6a6791a7288aeebe263af91475
Zect <57027411+sas08@users.noreply.github.com>
2021-08-30 17:35:04 +0900

    [Add]: 曲をjsonに追加する機構
    - 追加できるけど、重複の確認がうまくいってない

--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,7 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const request = require('request');
+const fs = require('fs');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -12,23 +14,72 @@ client.on('interactionCreate', async interaction => {
 
 	const { commandName } = interaction;
 
-  switch (commandName) {
-    case 'add':
-      await addUrl(interaction);
-      break;
-    default:
-      break;
-  }
-});
+	if (!commandName === 'add') {return;}
 
-async function addUrl (interaction) {
   const url = interaction.options.getString('url')
   if (!url) {
     await interaction.reply("楽曲urlを指定してね\nusage: /add [ Youtube_url | NicoNico_url ]")
     return;
   }
-  
-  await interaction.reply(`${url} を追加するよー`);
+	
+	request.get({
+		uri: 'https://vocadb.net/api/songs',
+		headers: {'Content-type': 'text/plain'},
+		qs: {
+			query: url,
+			maxResults: 1,
+			fields: 'Lyrics'
+		},
+		json: true
+	}, async function(err, req, data){
+		// console.info(data);
+
+		try {
+			const song = data['items'][0];
+			const lylic = song['lyrics'][0]['value'].replace('\r', '').split('\n\n')[0];
+		} catch (err) {
+			await interaction.reply(`${url}\nこの楽曲は追加できませんでした`);
+			return;
+		}
+		if (url.startsWith('https://www.nicovideo.jp/watch/')) {
+			await addToJson(url, 'https://www.nicovideo.jp/watch/', 'niconico', interaction);
+		} else if (url.startsWith('http://nico.ms/')) {
+			await addToJson(url, 'http://nico.ms/', 'niconico', interaction);
+		} else if (url.startsWith('https://www.youtube.com/watch?v=')) {
+			await addToJson(url, 'https://www.youtube.com/watch?v=', 'youtube', interaction);
+		} else if (url.startsWith('https://youtu.be/')) {
+			await addToJson(url, 'https://youtu.be/', 'youtube', interaction);
+		}
+		await interaction.reply(`__**${data.items[0].name}**__ が正常に追加されました`);
+		console.info(`${data.items[0].name} has been added.`)
+	}); 
+
+});
+
+async function addToJson (url, baseUrl, type, interaction) {
+	const { youtube, niconico } = require('../src/question.json')
+	const songid = url.replace(baseUrl, '');
+	switch (type) {
+		case 'niconico':
+			if (niconico.indexOf(songid) === -1) {
+				await interaction.reply('この曲は既に追加されてます');
+				return;
+			}
+			niconico.push(songid);
+			break;
+		case 'youtube':
+			if (youtube.indexOf(songid) === -1) {
+				await interaction.reply('この曲は既に追加されてます');
+				return;
+			}
+			youtube.push(songid);
+			break;
+		default:
+			await interaction.reply('エラーが発生しました。')
+			break;
+	}
+	const dataJ = {youtube: youtube, niconico: niconico};
+	fs.writeFileSync('../src/question.json', JSON.stringify(dataJ, null, 2));
 }
 
 client.login(token);
