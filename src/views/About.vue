8d99735ddfda120ea0015b842dbe0b91432e6368
Zect <57027411+sas08@users.noreply.github.com>
2021-08-31 22:16:39 +0900

    [Test]: GET, POST, DELETEの実験
    - 全部うまくいった！

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="app">
-    <dl v-for="(data, index) in datas" :key="data.id" >
-      <p>{{ index }}. {{ data.name }}: {{ data.answer }}</p>
-    </dl>
-  </div>
+  <v-app>
+    <div id="app">
+      <v-btn v-on:click="send">追加</v-btn>
+      <dl v-for="(data, index) in datas" :key="data.id" >
+        <p>{{ index }}. {{ data.name }}: {{ data.answer }}</p>
+      </dl>
+      <v-btn v-on:click="dele">削除</v-btn>
+    </div>
+  </v-app>
 </template>
 
 <script>
@@ -30,6 +34,22 @@ export default {
         console.info(datas)
         return datas
       }).then(this.setAnswers)
+    },
+    send () {
+      axios.post('http://localhost:8050' + '/answers', {
+        answer: 'ばらばら',
+        id: 711804442374307870,
+        name: 'Vocaleague'
+      }).then(function (response) {
+        console.log(response.data)
+      })
+      this.got()
+    },
+    dele () {
+      axios.delete('http://localhost:8050' + '/answers/' + 711804442374307870).then(res => {
+        console.log(res.data)
+      })
+      this.got()
     },
     setAnswers (res) {
       console.info(res)
