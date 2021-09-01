73af2517525b3e623e1f1a1f832bbfa3f5140e8a
Zect <57027411+sas08@users.noreply.github.com>
2021-09-01 21:25:38 +0900

    [Update]: Vue -> Vue3
    - バージョンアップにより、全ファイルデフォルトになった
    - 元ファイルたちは避難してるから、それを整形して追加していく

--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-// import store from './store'
 
-Vue.config.productionTip = false
-
-Vue.use(require('vue-cookies'))
-
-new Vue({
-  router,
-  vuetify,
-  // store,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
