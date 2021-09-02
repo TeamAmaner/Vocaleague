7fc0b0bbe19b7900bf2ff443c4ed539b3e8046e2
Zect <57027411+sas08@users.noreply.github.com>
2021-09-02 17:06:23 +0900

    [Add]: `element plus`を採用

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
 import router from './router'
+import store from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.use(ElementPlus)
+
+app.mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,104 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <el-container>
+    <el-main>
+
+      <div class="todo-list">
+        <h1>Vue 3 Todo App</h1>
+        <el-form @submit.prevent="addNewTodo">
+          <el-input v-model="newTodo" name="newTodo" placeholder="タスクを入力"></el-input>
+        <el-button @click="addNewTodo">追加</el-button>
+        </el-form>
+
+        <el-button @click="removeAllTodos">全て削除</el-button>
+        <el-button @click="markAllDone">全て完了</el-button>
+
+        <ul>
+          <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
+            <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">{{todo.content}}</h3>
+            <div>
+              <el-button @click="removeTodo(index)">削除</el-button>
+              <el-button @click="toggleDone(todo)">完了</el-button>
+            </div>
+          </li>
+        </ul>
+
+      </div>
+
+    </el-main>
+  </el-container>
 </template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    // newTodoをセット
+    const newTodo = ref('');
+    // todosをセット
+    const todos = ref([]);
+    // todoを作成
+    const addNewTodo = () => {
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      // newTodoの値を空にする
+      newTodo.value = '';
+    }
+    // todoのdoneのTrueとFalseを入れ替えることで、:classを動的に変化させる
+    const toggleDone = (todo) => {
+      todo.done = !todo.done;
+    }
+    // 削除
+    const removeTodo = (index) => {
+      todos.value.splice(index, 1);
+    }
+    // 全てTrue
+    const markAllDone = () => {
+      todos.value.forEach((todo) => todo.done = true);
+    }
+    // 全て削除
+    const removeAllTodos = () => {
+      todos.value = [];
+    }
+
+    return {
+      todos,
+      newTodo,
+      addNewTodo,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      removeAllTodos,
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+input {
+  width: 30vw !important;
+}
+h3 {
+  margin-right: 20px;
+}
+li {
+  width: 30vw;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+}
+.todo {
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
+}
+</style>
